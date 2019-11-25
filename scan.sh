cd6466236ae18428358c7e3c36f347a733c16984
Lubor PETR <lubor.petr@gemalto.com>
2019-11-25 18:48:05 +0100

    Added exit code handling

--- scan.sh
+++ scan.sh
@@ -34,9 +34,15 @@ done
 if [ -n "${SCAN_DIR}" ] && [ -n "${LOG_FILE}" ] ; 
 then
 	echo "Scanning directory $SCAN_DIR"
-	/usr/bin/clamscan -d /clamavdb -vr ${SCAN_DIR} --log=$LOG_FILE
-        echo "Scan completed, detailed report in $LOG_FILE"
+	if /usr/bin/clamscan -d /clamavdb -vr ${SCAN_DIR} --log=$LOG_FILE; then
+        	echo "Scan completed, detailed report in $LOG_FILE"
+        	exit 0
+	else
+		echo "Scan detected infected files, see $LOG_FILE for details"
+		exit 1
+	fi
 else
 	echo "Missing mandatory parameters"
+	exit 1
 fi
 
