8a28bb9a1263c35504be98fa13f542a8b4ae35f8
Lubor PETR <lubor.petr@thalesgroup.com>
2020-01-21 16:04:38 +0100

    DB integrity check

--- db-update.sh
+++ db-update.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 MAX_RETRIES=5
+DATA_DIR="/clamavdb"
 
 function updatedb() {
-	/usr/bin/freshclam --datadir="/clamavdb" --config-file=/etc/clamav/freshclam.conf
+	/usr/bin/freshclam --datadir=$DATA_DIR --config-file=/etc/clamav/freshclam.conf
+}
+
+function check-db-integrity() {
+ if [[ -f $DATA_DIR/bytecode.cvd && -f $DATA_DIR/daily.cvd && -f $DATA_DIR/main.cvd ]];then
+    echo "AV DB integrity check is fine"
+    return 0
+ fi
+ echo "AV DB integrity check has failed, some files are missing."
+ return 1
 }
 
 for (( i=1; i<MAX_RETRIES; i++ ));
@@ -11,7 +21,9 @@ do
  echo "DB Update attempt no.: $i"
  updatedb
  EXIT_CODE=$?
- if [ $EXIT_CODE -eq 0 ]; then 
+ check-db-integrity
+ INTEGRITY_CHECK=$?
+ if [[ $EXIT_CODE -eq 0 && $INTEGRITY_CHECK -eq 0 ]]; then 
   echo  "DB updated succesfully"
   exit 0
  fi
